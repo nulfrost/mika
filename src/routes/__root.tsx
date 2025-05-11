cb72bc35c530a5b2f1e59f95ef7e50e5588a2632
nulfrost <khadane.miller@gmail.com>
2025-05-11 16:57:02 -0400

    cleanu

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,57 +1,52 @@
 import {
-	HeadContent,
-	Outlet,
-	Scripts,
-	createRootRoute,
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRoute,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-
-import Header from "../components/Header";
-
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
-	head: () => ({
-		meta: [
-			{
-				charSet: "utf-8",
-			},
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
-				title: "TanStack Start Starter",
-			},
-		],
-		links: [
-			{
-				rel: "stylesheet",
-				href: appCss,
-			},
-		],
-	}),
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStack Start Starter",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
 
-	component: () => (
-		<RootDocument>
-			<Header />
-
-			<Outlet />
-			<TanStackRouterDevtools />
-		</RootDocument>
-	),
+  component: () => (
+    <RootDocument>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </RootDocument>
+  ),
 });
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<HeadContent />
-			</head>
-			<body>
-				{children}
-				<Scripts />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
 }
